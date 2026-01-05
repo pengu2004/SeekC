c448aaef4fcf6c442ec57ddbb32966e597123315
pengu2004 <tejusria02@gmail.com>
2026-01-06 00:38:33 +0530

    add function to check for text file

--- scanner.c
+++ scanner.c
@@ -1,16 +1,36 @@
-#include <stdio.h>
-
 #include <dirent.h>
+#include <stdio.h>
+#include <string.h>
 #include <sys/types.h>
+
+int check_text_file(const char *name) {
+  const char *extension = strchr(name, '.');
+  if (!extension) {
+    return 0;
+  }
+  const char *extentions[] = {".csv", ".c", ".h"};
+  int count =
+      sizeof(extentions) / sizeof(extentions[1]); // find the lenght of array
+  for (int i = 0; i < count; i++) {
+    if (strcmp(extension, extentions[i]) == 0) {
+      return 1;
+    }
+  }
+  return 0;
+}
 
 void scan_dir(const char *path) {
   printf("The path %s", path);
-  DIR *openfile = opendir(path); //open the file
+  DIR *openfile = opendir(path); // open the file
   while (1) {
-    struct dirent *pfile = readdir(openfile); //read the file the fro the file stream
-    if (pfile == NULL) { //check if the readdir returns null which indicates end of dir
+    struct dirent *pfile =
+        readdir(openfile); // read the file the fro the file stream
+    if (pfile ==
+        NULL) { // check if the readdir returns null which indicates end of dir
       break;
     }
-    printf("%s\n", pfile->d_name);
+    if (check_text_file(pfile->d_name)) {
+      printf("%s\n", pfile->d_name);
+    }
   }
 }
